19be0c1d4f165c4b45d8dd8352848c110abd488c
omkar-2483 <kastureomkar03@gmail.com>
2024-01-14 13:01:56 +0530

    updated helloworld

--- helloworld.py
+++ helloworld.py
@@ -7,3 +7,13 @@ print("welcome"+ name)
 '''outpute:
 welcome omkar
 welcomeomkar'''
+
+#accept two integers and display their sum
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+print("Sum= ",a+b)
+
+'''output:
+Enter first number: 4
+Enter second number: 7
+Sum=  11'''
