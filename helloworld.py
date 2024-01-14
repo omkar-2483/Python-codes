6d5be56dedbead6e920aee5112c5c76265bd8001
omkar-2483 <kastureomkar03@gmail.com>
2024-01-14 12:51:08 +0530

    helloworld modifies

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+#write a program that asks the user for his name and welcomes him
+
+name=input("Enter your name: ")
+print("welcome", name)
+print("welcome"+ name)
+
+'''outpute:
+welcome omkar
+welcomeomkar'''
